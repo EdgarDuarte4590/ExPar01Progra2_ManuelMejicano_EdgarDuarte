3bef600da8dedac471f2b846c85311f26f8abe95
EdgarDuarte4590 <emmanuelduale2006@gmail.com>
2025-05-02 00:14:17 -0600

    clase guarda

--- ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/modelo/Guarda.java
+++ ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/modelo/Guarda.java
@@ -1,5 +1,33 @@
 package modelo;
 
 public class Guarda {
+    String nombre;
+    int ID;
+    String contrasena;
+
+    
+    public Guarda(String nombre, int iD, String contrasena) {
+        this.nombre = nombre;
+        ID = iD;
+        this.contrasena = contrasena;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getID() {
+        return ID;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 
 }
