a735d8167fd26388ddf89e1d1ebe422f9806dba8
manuel-mejicano <manuelortizmejicano100@gmail.com>
2025-05-01 23:51:46 -0600

    modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/bin/App.class modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/bin/modelo/Guarda.class 	modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/controlador/Controlador.java

--- ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/controlador/Controlador.java
+++ ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/controlador/Controlador.java
@@ -2,10 +2,9 @@ package controlador;
 
 import java.util.ArrayList;
 
-import modelo.FuenteIngreso;
 
 public class Controlador {
-    ArrayList<FuenteIngreso> ingresos = new ArrayList<>();
+  
     
 
 }
