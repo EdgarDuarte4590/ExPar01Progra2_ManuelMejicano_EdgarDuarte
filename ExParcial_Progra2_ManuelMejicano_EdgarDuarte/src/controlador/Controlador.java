5ece690b206b74829b4ba30bba73d03acffd94ee
manuel-mejicano <manuelortizmejicano100@gmail.com>
2025-05-02 01:59:19 -0600

    modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/bin/controlador/Controlador.class modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/bin/modelo/Guarda.class modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/bin/modelo/Ingreso.class modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/bin/modelo/IngresoExterno.class modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/bin/modelo/Salida.class modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/bin/vista/MenuPrincipal.class modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/bin/vista/administrador/VistaAdmin.class modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/controlador/Controlador.java modified:   ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/vista/administrador/VistaAdmin.java

--- ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/controlador/Controlador.java
+++ ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/controlador/Controlador.java
@@ -2,9 +2,31 @@ package controlador;
 
 import java.util.ArrayList;
 
+import modelo.Estudiante;
+import modelo.Guarda;
+
 
 public class Controlador {
-  
+      private ArrayList<modelo.Administrador> administradores;
+        private ArrayList<Guarda> oficiales;
+        private ArrayList<Estudiante> estudiantes;
+        private vista.administrador.VistaAdmin vistaAdmin;
+        private vista.oficiales.VistaOficiales vistaOficiales;
+        private vista.estudiantes.VistaEstudiante vistaEstudiante;
     
+        public Controlador() {
+            administradores = new ArrayList<>();
+            oficiales = new ArrayList<>();
+            estudiantes = new ArrayList<>();
+            vistaAdmin = new vista.administrador.VistaAdmin();
+            vistaOficiales = new vista.oficiales.VistaOficiales();
+            vistaEstudiante = new vista.estudiantes.VistaEstudiante();
+        }
+    
+        public void iniciar() {
+            // Aquí puedes agregar la lógica para iniciar la aplicación
+            // Por ejemplo, mostrar la vista del administrador
+            vistaAdmin.mostrar();
+        }   
 
 }
--- ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/vista/administrador/VistaAdmin.java
+++ ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/vista/administrador/VistaAdmin.java
@@ -1,8 +1,14 @@
 package vista.administrador;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 
 public class VistaAdmin extends JFrame{
+
+    JTabbedPane tabbedPane = new JTabbedPane();
     
         public VistaAdmin() {
             setTitle("Vista Administrador");
@@ -20,7 +26,80 @@ public class VistaAdmin extends JFrame{
         }
     
         private void inicializarComponentes() {
+            JTabbedPane tabbedPane = new JTabbedPane();
+            tabbedPane.setBounds(0, 0, 1366, 720); // Establecer el tamaño del JTabbedPane
+            super.add(tabbedPane); // Agregar el JTabbedPane a la ventana
+           tabbedPane.add("Menu Administrativo",panelAdministradores()) ;
+              tabbedPane.add("Oficiales", panelOficiales()); // Agregar el panel de oficiales al JTabbedPane
 
         }
-            // Aquí puedes agregar los componentes específicos de la vista del administrador
+        
+        public JPanel panelAdministradores() {
+            JPanel panelAdministradores = new JPanel();
+            panelAdministradores.setLayout(null); // Establecer el diseño nulo para el panel de administradores
+            panelAdministradores.setBounds(0, 0, 1366, 720); // Establecer el tamaño del panel de administradores
+            
+            JLabel label = new JLabel("Bienvenido Admin");
+            label.setBounds(40, 50, 500, 30); // Establecer la posición y el tamaño del JLabel
+            label.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 14)); // Establecer la fuente del JLabel
+            panelAdministradores.add(label); 
+
+
+            return panelAdministradores;
+        }
+
+        public JPanel panelOficiales() {
+            JPanel panelOficiales = new JPanel();
+            panelOficiales.setLayout(null); // Establecer el diseño nulo para el panel de oficiales
+            panelOficiales.setBounds(0, 0, 1366, 720); // Establecer el tamaño del panel de oficiales
+            
+            JLabel label = new JLabel("Agrega y visualiza los oficiales");
+            label.setBounds(40, 50, 500, 30); // Establecer la posición y el tamaño del JLabel
+            label.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 14)); // Establecer la fuente del JLabel
+            panelOficiales.add(label); 
+
+            JLabel label2 = new JLabel("Nombre completo de Oficial:");
+            label2.setBounds(40, 100, 500, 30); 
+            panelOficiales.add(label2); // Agregar el JLabel al panel de oficiales
+
+            JTextField textFieldNombre = new JTextField();
+            textFieldNombre.setBounds(250, 100, 200, 30); // Establecer la posición y el tamaño del JTextField
+            panelOficiales.add(textFieldNombre); // Agregar el JTextField al panel de oficiales
+
+            JLabel label3 = new JLabel("Número de identificación:");
+            label3.setBounds(40, 150, 500, 30);
+            panelOficiales.add(label3); // Agregar el JLabel al panel de oficiales
+
+            JTextField textFieldID = new JTextField();
+            textFieldID.setBounds(250, 150, 200, 30); // Establecer la posición y el tamaño del JTextField
+            panelOficiales.add(textFieldID); // Agregar el JTextField al panel de oficiales
+
+            JTextField textFieldTelefono = new JTextField();
+            textFieldTelefono.setBounds(250, 200, 200, 30); // Establecer la posición y el tamaño del JTextField
+            panelOficiales.add(textFieldTelefono); // Agregar el JTextField al panel de oficiales
+
+            JLabel label4 = new JLabel("Número de teléfono:");
+            label4.setBounds(40, 200, 500, 30); // Establecer la posición y el tamaño del JLabel
+            panelOficiales.add(label4); // Agregar el JLabel al panel de oficiales
+
+            JLabel label5 = new JLabel("ID Acceso:");
+            label5.setBounds(40, 250, 500, 30); // Establecer la posición y el tamaño del JLabel
+            panelOficiales.add(label5); // Agregar el JLabel al panel de oficiales
+
+            JTextField textFieldIDAcceso = new JTextField();
+            textFieldIDAcceso.setBounds(250, 250, 200, 30); // Establecer la posición y el tamaño del JTextField
+            panelOficiales.add(textFieldIDAcceso); // Agregar el JTextField al panel de oficiales
+
+
+            JLabel label6 = new JLabel("Contraseña:");
+            label6.setBounds(40, 300, 500, 30); // Establecer la posición y el tamaño del JLabel
+            panelOficiales.add(label6); // Agregar el JLabel al panel de oficiales
+
+            JTextField textFieldContrasena = new JTextField();
+            textFieldContrasena.setBounds(250, 300, 200, 30); // Establecer la posición y el tamaño del JTextField
+            panelOficiales.add(textFieldContrasena); // Agregar el JTextField al panel de oficiales
+
+
+            return panelOficiales;
+        }
 }
