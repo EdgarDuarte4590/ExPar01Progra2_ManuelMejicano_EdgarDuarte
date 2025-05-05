a7d85e9a122dca7da27c651fa133085ba74f2718
manuel-mejicano <manuelortizmejicano100@gmail.com>
2025-05-04 20:34:54 -0600

    VISTA

--- ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/vista/estudiantes/VistaEstudiante.java
+++ ExParcial_Progra2_ManuelMejicano_EdgarDuarte/src/vista/estudiantes/VistaEstudiante.java
@@ -1,14 +1,26 @@
 package vista.estudiantes;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+
+import controlador.Controlador;
+
 
 public class VistaEstudiante  extends JFrame{
-    public VistaEstudiante() {
+    Controlador controlador;
+
+    JTabbedPane tabbedPane;
+    public VistaEstudiante(Controlador controlador) {
         setTitle("Vista Estudiante");
         setSize(1366, 720);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null); 
         setLayout(null); 
+        this.controlador = controlador; 
+    }
+
+    public void inicializarComponentes(){
+        tabbedPane = new JTabbedPane();
     }
 
     public void mostrar() {
